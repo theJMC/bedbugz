6c132943cd393517eaa178f17ba89a43841306c4
James <james@jamesmcc.co.uk>
2025-09-23 14:09:57 +0100

    new ai stuff

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ API_BASE_URL = "https://api.inaturalist.org/v1"
 # API_PROJECT_ID = "bu-nature-hackathon-2025"
 API_PROJECT_ID = "bournemouth-university-campus-biodiversity-network"
 
-req = r.get(API_BASE_URL + f"/observations?project_id={API_PROJECT_ID}&order=desc&order_by=created_at")
+API_CALL_LIMIT = 10
+
+req = r.get(API_BASE_URL + f"/observations?project_id={API_PROJECT_ID}&order=desc&order_by=created_at&per_page={API_CALL_LIMIT}")
 res = req.json()
 
 for i in res["results"]:
@@ -14,4 +16,6 @@ for i in res["results"]:
     except TypeError:
         print(f"{i['uuid']} - species: {i['species_guess']}")
 
-print(len(res["results"]))
+# print(len(res["results"]))
+
+print(res["results"][0]["taxon"]["default_photo"]["medium_url"])
