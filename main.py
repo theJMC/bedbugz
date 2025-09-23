cacfda12a811392d909ff9b0ffa14bff8572923e
James <james@jamesmcc.co.uk>
2025-09-23 13:05:30 +0100

    Initial Commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import requests as r
+
+API_BASE_URL = "https://api.inaturalist.org/v1"
+
+API_PROJECT_ID = "bu-nature-hackathon-2025"
+
+req = r.get(API_BASE_URL + f"/observations?project_id={API_PROJECT_ID}&order=desc&order_by=created_at")
+res = req.json()
+
+for i in res["results"]:
+    print(i)
