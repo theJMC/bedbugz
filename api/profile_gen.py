62ac98481e24dea94392148fd52f6590e324fc2a
James <james@jamesmcc.co.uk>
2025-09-25 16:35:17 +0100

    new profiles and json enhancement

--- api/profile_gen.py
+++ api/profile_gen.py
@@ -8,7 +8,9 @@ API_CALL_LIMIT = 10
 API_TAXAS = "Actinopterygii%2CAnimalia%2CAmphibia%2CArachnida%2CAves%2CChromista%2CFungi%2CInsecta%2CMammalia%2CMollusca%2CReptilia%2CProtozoa"
 
 from openai import OpenAI
+
 client = OpenAI()
+
 
 class Profile:
     def __init__(self, name: str, species: str, images: list[str], description: str, prompts: list[dict[str, str]]):
@@ -25,17 +27,20 @@ class Profile:
         return {
             "name": self.name,
             "species": self.species,
-            "images": self. images,
+            "images": self.images,
             "description": self.description,
             "prompts": self.prompts
-          },
+        },
+
 
 def add_new_profile(page: int):
-    req = r.get(API_BASE_URL + f"/observations?project_id={API_PROJECT_ID}&order=desc&order_by=created_at&iconic_taxa={API_TAXAS}&per_page=1&page={page}")
+    req = r.get(
+        API_BASE_URL + f"/observations?project_id={API_PROJECT_ID}&order=desc&order_by=created_at&iconic_taxa={API_TAXAS}&per_page=1&page={page}")
     # req = r.get(
-        # API_BASE_URL + f"/observations?project_id={API_PROJECT_ID}&order=desc&order_by=created_at&per_page=1")
+    # API_BASE_URL + f"/observations?project_id={API_PROJECT_ID}&order=desc&order_by=created_at&per_page=1")
     if req.json()["total_results"] < page:
         raise Exception(f"Page {page} does not exist")
+
     try:
         bug = req.json()["results"][0]
     except IndexError:
@@ -43,16 +48,22 @@ def add_new_profile(page: int):
     result = []
     try:
         name = bug["taxon"]["preferred_common_name"]
-    except TypeError:
-        name = "no_name"
+    except KeyError:
+        name = bug["taxon"]["name"]
 
-    try:
-        default_photo = [bug["taxon"]["default_photo"]["url"]]
-    except TypeError:
-        default_photo = []
+    photos = []
+    # try:
+    for photo in bug["photos"]:
+        photos.append(photo["url"])
+    # except TypeError:
+    #     pass
 
     # description = bug["taxon"]["description"]
-    description = "fake description"
+    description = f"Fun Fact! It's a {name}"
+
+    # generate stuff
+    spec_req = r.get(API_BASE_URL + "/taxa/" + str(bug["taxon"]["parent_id"]))
+    spec_name = spec_req.json()["results"][0]["name"]
 
     response = client.responses.create(
         model="o4-mini",
@@ -63,22 +74,23 @@ def add_new_profile(page: int):
     )
     prompts = json.loads(" ".join(response.output_text.split()))
 
-    return Profile(name, "test", default_photo, description, prompts)
+    return Profile(name, spec_name, photos, description, prompts)
 
 
 def write_to_file(results):
     with open("new_profiles.json", "w+") as json_file:
         json.dump(results, json_file)
 
+
 if __name__ == "__main__":
     results = []
     try:
         for i in range(20):
-            new = add_new_profile(i+1)
+            print(i)
+            new = add_new_profile(i + 1)
             print(new.to_json()[0])
             results.append(new.to_json()[0])
-    except Exception as e:
-        print(e)
+    except KeyboardInterrupt as e:
+        pass
     finally:
         write_to_file(results)
-
