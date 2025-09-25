ce5ffa09501f72c5aa35891269f91d03cab0dc1b
JesssseL <146840057+JesssseL@users.noreply.github.com>
2025-09-25 15:12:30 +0100

    add title + meta description

--- bedbugz/nuxt.config.ts
+++ bedbugz/nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/devtools', '@nuxt/image']
+  modules: ['@nuxt/eslint', '@nuxt/devtools', '@nuxt/image'],
+  app: {
+    head: {
+      title: 'BedBugZ',
+      meta: [
+        { name: 'description', content: 'Welcome to BedBugz! A bug-inspired dating simulator designed to teach users about the wildlife and nature around Bournemouth University campuses.' }
+      ]
+    }
+  }
 })
