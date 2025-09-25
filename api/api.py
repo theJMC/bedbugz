031bc6a7d7464ad302cb68052a11acf42a6be16f
James <james@jamesmcc.co.uk>
2025-09-25 12:45:39 +0100

    more stuff in one respose - plus CORS fix

--- api/api.py
+++ api/api.py
@@ -1,16 +1,28 @@
 from fastapi import FastAPI
 from openai import OpenAI
+from fastapi.middleware.cors import CORSMiddleware
 client = OpenAI()
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/{bug}")
 async def root(bug):
     response = client.responses.create(
-        model="gpt-5-nano",
-        input="You are pretending to be an insect bug on tinder. You are a [bug]. Please can you generate a message \
+        model="o4-mini",
+        input="Please generate 6 of the below response. You are pretending to be an insect bug on tinder. You are a [bug]. Please can you generate a message \
         from you, the bug, to another bug. Can you try to keep the responses concise please. Please respond exactly in the format \
         {'bugMsg': 'MESSAGE','responses': \
         {'good': 'GOOD_RESPONSE','medium': 'MEDIUM_RESPONSE','bad': 'BAD_RESPONSE'}}".replace("[bug]", bug)
     )
-    return response.output_text
+    results = []
+    for res in response.output_text.split("\n"):
+        results.append(res)
+    return results
 
