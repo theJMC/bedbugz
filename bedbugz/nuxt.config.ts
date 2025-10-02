c36f01719e4dffc49f569f1872b3115fadd3e7d0
James <james@jamesmcc.co.uk>
2025-10-02 15:23:36 +0100

    Added Umami to the site

--- bedbugz/nuxt.config.ts
+++ bedbugz/nuxt.config.ts
@@ -2,5 +2,23 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/devtools', '@nuxt/image']
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/devtools',
+    '@nuxt/image',
+    'nuxt-umami'
+  ],
+  umami: {
+    id: '5ad55f3f-72f5-4721-9310-e4cee3a32f95',
+    host: 'https://umami.thejmc.net',
+    autoTrack: true,
+    // proxy: 'cloak',
+    // useDirective: true,
+    // ignoreLocalhost: true,
+    // excludeQueryParams: false,
+    // domains: ['cool-site.app', 'my-space.site'],
+    // customEndpoint: '/my-custom-endpoint',
+    // enabled: false,
+    // logErrors: true,
+  },
 })
